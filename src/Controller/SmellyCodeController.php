9ae41a3e985912313c93a3048cc1b115076893e2
TBoileau <t-boileau@email.com>
2021-11-07 14:26:31 +0100

    feat: create gist smelly code (#15)

---
+++ src/Controller/SmellyCodeController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Gist;
+use App\Form\GistType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(name: 'smelly_code_')]
+#[IsGranted('ROLE_USER')]
+final class SmellyCodeController extends AbstractController
+{
+    #[Route('/new', name: 'new')]
+    public function new(Request $request): Response
+    {
+        $gist = new Gist();
+
+        $form = $this->createForm(GistType::class, $gist)->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->persist($gist);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('smelly_code/new.html.twig', ['form' => $form->createView()]);
+    }
+}
--- src/Entity/Gist.php
+++ src/Entity/Gist.php
@@ -5,11 +5,17 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Url;
 
 #[ORM\Entity]
 class Gist extends SmellyCode
 {
     #[ORM\Column]
+    #[NotBlank]
+    #[Url]
+    #[Regex(pattern: '/^https:\/\/gist\.github\.com\/.*\/\w+$/')]
     private string $url;
 
     public function getUrl(): string
